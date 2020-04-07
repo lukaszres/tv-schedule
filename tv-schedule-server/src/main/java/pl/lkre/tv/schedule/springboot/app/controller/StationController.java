759fe56f86eeb4149654b2b6bc34077095f65e74
lkre <lukasz.res@gmail.com>
2020-04-07 06:56:06 +0200

    add StationController

---
+++ tv-schedule-server/src/main/java/pl/lkre/tv/schedule/springboot/app/controller/StationController.java
@@ -0,0 +1,44 @@
+package pl.lkre.tv.schedule.springboot.app.controller;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.lkre.tv.schedule.model.Station;
+import pl.lkre.tv.teleman.StationFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+@Controller
+@RequestMapping("/station")
+public class StationController {
+
+
+    @GetMapping
+    @ResponseBody
+    public String station(Model model) throws IOException {
+        StationFactory stationFactory = new StationFactory();
+
+
+        final URL url = new URL("https://www.teleman.pl/program-tv/stacje/TVP-1");
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        String line;
+        final StringBuilder stringBuilder = new StringBuilder();
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(System.lineSeparator());
+        }
+
+        final Document document = Jsoup.parse(stringBuilder.toString());
+        final Station station = stationFactory.build(document);
+
+        return station.toString();
+    }
+}
