ffd2af08f06598930b409956b1f989be4e815be8
lkre <lukasz.res@gmail.com>
2020-04-08 14:45:58 +0200

    StationsController change url mapping

--- tv-schedule-server/src/main/java/pl/lkre/tv/schedule/springboot/app/controller/StationsController.java
+++ tv-schedule-server/src/main/java/pl/lkre/tv/schedule/springboot/app/controller/StationsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.lkre.tv.persistance.StorageService;
@@ -18,23 +19,23 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 @Controller
-@RequestMapping("/station")
-public class StationController {
+@RequestMapping(value = "/stations/{stationName}")
+public class StationsController {
 
     private final StorageService storageService;
 
     @Autowired
-    public StationController(StorageService storageService) {
+    public StationsController(StorageService storageService) {
         this.storageService = storageService;
     }
 
     @GetMapping
     @ResponseBody
-    public String station(Model model) throws IOException {
+    public String station(@PathVariable("stationName") String stationName, Model model) throws IOException {
         StationFactory stationFactory = new StationFactory();
 
 
-        final URL url = new URL("https://www.teleman.pl/program-tv/stacje/TVP-1");
+        final URL url = new URL("https://www.teleman.pl/program-tv/stacje/" + stationName);
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
 
         String line;
